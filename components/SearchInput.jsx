4639fafb708673402b9c10fcc39899e1ceaf23ce
Tamas <tamas.fehertoi@gmail.com>
2024-04-21 11:59:07 -0500

    add trending jsx

--- components/SearchInput.jsx
+++ components/SearchInput.jsx
@@ -18,7 +18,7 @@ const SearchInput = ({
       <TextInput
         className="text-base mt-0.5 text-white flex-1 font-pregular"
         value={value}
-        placeholder={placeholder}
+        placeholder="Search for a video topic"
         placeholderTextColor="#7B7B8B"
         onChangeText={handleChangeText}
         secureTextEntry={title === "Password" && !showPassword}
